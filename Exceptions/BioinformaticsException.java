29e9c834ac4ed2f4c013fe15ee08eba585f72ee3
Soniya Lama <99881001+soniyalama@users.noreply.github.com>
2025-04-01 22:43:38 +0200

    Create BioinformaticsException.java

---
+++ Exceptions/BioinformaticsException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class BioinformaticsException extends Exception {
+
+	/**
+	 * A default serial version ID.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BioinformaticsException(String message) {
+		super(message);
+	}
+}
